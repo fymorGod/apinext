a66721793a49d9b91413df04ed8992161791851d
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-05-19 09:59:01 -0300

    iniciando em planetscale

---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    return res.status(200).json({
+        data:  [
+            {id: 1, name: 'Luiz'},
+            {id: 2, name: 'Maria'},
+            {id: 3, name: 'Katarina'},
+        ]
+    })
+  }
